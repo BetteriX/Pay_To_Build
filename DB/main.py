5868a36c282bc5ac2a54feeef5946422c3f2aff1
BetteriX <szabozsolt430@gmail.com>
2025-10-11 17:40:22 +0200

    fix

--- DB/main.py
+++ DB/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+import pypartpicker
 
 
 def main():
-    pass
+    pcpp = pypartpicker.Client()
+    part = pcpp.get_part("https://pcpartpicker.com/product/fN88TW")
+
+    for spec, value in part.specs.items():
+        print(f"{spec}: {value}")
+
+    print(part.cheapest_price)
+
 
 if __name__ == "__main__":
     main()
