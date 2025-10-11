09bcf0444f382d5ad0e149e9a50a92636a0d034e
BetteriX <szabozsolt430@gmail.com>
2025-10-11 18:55:24 +0200

    db

--- DB/main.py
+++ DB/main.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
+
+import os
+from dotenv import load_dotenv
 import pypartpicker
 import mysql.connector
 
 
 def main():
-    pcpp = pypartpicker.Client()
-    part = pcpp.get_part("https://pcpartpicker.com/product/fN88TW")
+    # pcpp = pypartpicker.Client()
+    # part = pcpp.get_part("https://pcpartpicker.com/product/fN88TW")
 
-    for spec, value in part.specs.items():
-        print(f"{spec}: {value}")
+    # for spec, value in part.specs.items():
+    #    print(f"{spec}: {value}")
 
-    print(part.cheapest_price)
+    # print(part.cheapest_price)
     print(mysql.connector.__version__)
+
+    load_dotenv()
+    db = mysql.connector.connect(
+        host=os.getenv("IP"),
+        port=os.getenv("PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    # Get a cursor
+    cur = db.cursor()
+
+    # Execute a query
+    cur.execute("SELECT CURDATE()")
+
+    # Fetch one result
+    row = cur.fetchone()
+    print("Current date is: {0}".format(row[0]))
+
+    # Close connection
+    db.close()
 
 
 if __name__ == "__main__":
