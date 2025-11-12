48df821715572efdadd7e7596b3940f45e3ddc29
Dani <hubai.dani@gmail.com>
2025-11-12 13:17:44 +0100

    Created Login and GuidePage + made Main buttons work

---
+++ UI/src/main/java/PayToBuild/App/LoginController.java
@@ -0,0 +1,4 @@
+package PayToBuild.App;
+
+public class LoginController {
+}
--- UI/src/main/java/PayToBuild/App/MainApplication.java
+++ UI/src/main/java/PayToBuild/App/MainApplication.java
@@ -2,6 +2,7 @@ package PayToBuild.App;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,11 +10,16 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("MainPage");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("MainPage");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
