0806fa30b3662dabfce036aad0bbd3323736a609
Dani <hubai.dani@gmail.com>
2025-11-12 12:28:06 +0100

    Created stylesheet and Controller for MainPage and connected them

--- UI/src/main/java/PayToBuild/App/MainApplication.java
+++ UI/src/main/java/PayToBuild/App/MainApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("David");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("MainPage");
         stage.setScene(scene);
         stage.show();
     }
---
+++ UI/src/main/java/PayToBuild/App/MainController.java
@@ -0,0 +1,16 @@
+package PayToBuild.App;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+
+public class MainController {
+    @FXML
+    private Button guidebutton;
+    @FXML
+    private Button loginbutton;
+    @FXML
+    private ScrollBar mainscroller;
+
+
+}
